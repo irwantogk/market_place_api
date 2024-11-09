729b9f7a5a0e52e7c5bae4cd1c305ddc91e82eb4
irwantogk <irwantowibowo1992@gmail.com>
2024-11-09 20:57:56 +0700

    Testing for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
 end
